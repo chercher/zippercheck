5890ff15f3caf91731a6666c64ef4a1bf4eb26be
wang.jing <pansy.wang@dianping.com>
2015-01-21 21:20:03 +0800

    run script

---
+++ bin/zippercheck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export LIBJARS=/usr/local/hadoop/hadoop-release/share/hadoop/common/hadoop-common-2.4.1-dp_1.jar,/usr/local/hadoop/hadoop-release/share/hadoop/m
+apreduce/hadoop-mapreduce-client-app-2.4.1-dp_1.jar,/usr/local/hadoop/hadoop-release/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.4.1-d
+p_1.jar,/usr/local/hadoop/hadoop-release/share/hadoop/common/lib/commons-lang-2.6.jar
+export HADOOP_CLASSPATH=/usr/local/hadoop/hadoop-release/share/hadoop/common/hadoop-common-2.4.1-dp_1.jar:/usr/local/hadoop/hadoop-release/share
+/hadoop/mapreduce/hadoop-mapreduce-client-app-2.4.1-dp_1.jar:/usr/local/hadoop/hadoop-release/share/hadoop/mapreduce/hadoop-mapreduce-client-cor
+e-2.4.1-dp_1.jar:/usr/local/hadoop/hadoop-release/share/hadoop/common/lib/commons-lang-2.6.jar
+
+hadoop jar zippercheck-1.0-SNAPSHOT.jar com.dianping.data.ZipperCheck -libjars ${LIBJARS} $@
